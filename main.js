d291f4ff3a3e84b8f0d7653da43aed37c8da0411
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-03 20:20:59 -0500

    commit tree c00274e387bd76027f7a73a650bb4f4019520e74

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  let hearts = document.querySelectorAll('span.like-glyph');
+  for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+      mimicServerCall().then( () => toggleHeart(heart) ).catch(error => displayError(error));
+    });
+  };
+});
+
+function displayError(error){
+  document.getElementById('modal').classList.remove('hidden');
+  document.getElementById('modal-message').innerText = error;
+};
+
+function toggleHeart(heart){
+  if (heart.innerText == EMPTY_HEART){
+    heart.innerText = FULL_HEART;
+    heart.classList.add('activated-heart');
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
 
 
 
